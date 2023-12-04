e3c52a32fc4d8d3a892855f20824c42bc6ec026a
Mehul Patangya <mehuljainind@gmail.com>
2023-12-04 16:42:43 +0530

    Added timeout in creation of fn, correct the export_lambda_code

--- lambda.py
+++ lambda.py
@@ -1,8 +1,9 @@
+import json
 import boto3
-import wget
+import requests
 import os
 
-def create_function(function_name, function_handler, function_runtime):
+def create_function(function_name,function_timeout, function_handler, function_runtime):
     sts_connection = boto3.client('sts')
     acct_b = sts_connection.assume_role(
         RoleArn="arn of the other account lambda role giving full access to the source account ",  # lambda account role
@@ -10,7 +11,7 @@ def create_function(function_name, function_handler, function_runtime):
     )
 
     ACCESS_KEY = acct_b['Credentials']['AccessKeyId']
-    SECRET_KEY = acct_b['Credentials']['SecretAccessKeyId']
+    SECRET_KEY = acct_b['Credentials']['SecretAccessKey'] 
     SESSION_TOKEN = acct_b['Credentials']['SessionToken']
     client = boto3.client(
         'lambda',
@@ -22,6 +23,7 @@ def create_function(function_name, function_handler, function_runtime):
     try:
         client.create_function(
             FunctionName=function_name,
+            Timeout=function_timeout,
             Handler=function_handler,
             Runtime=function_runtime,
             Role="arn of the lambda role that has the only execute permission",  # resp_gf["Configuration"]["Role"],
@@ -29,7 +31,6 @@ def create_function(function_name, function_handler, function_runtime):
         )
         os.remove('./' + function_name + '.zip')
         print("Create Function: " + function_name)
-
     except Exception as e:
         print("A problem occurred while creating function : " + function_name + "-----" + str(e))
 
@@ -43,6 +44,7 @@ def get_function_from_source():
         functions = response["Functions"]
         for function in functions:
             function_name = str(function["FunctionName"])
+            function_timeout = function["Timeout"]
             function_handler = function["Handler"]
             function_runtime = function["Runtime"]
             # Export configuration
@@ -50,7 +52,7 @@ def get_function_from_source():
             # Export code
             export_lambda_code(source_lambda_client, function_name)
             # Create function in the target account
-            create_function(function_name, function_handler, function_runtime)
+            create_function(function_name, function_timeout, function_handler, function_runtime)
 
 
 def export_lambda_configuration(lambda_client, function_name):
@@ -60,12 +62,14 @@ def export_lambda_configuration(lambda_client, function_name):
 
 
 def export_lambda_code(lambda_client, function_name):
-    response = lambda_client.get_function(FunctionName=function_name)
-    code_data = response['Code']
-    with open('./' + function_name + '.zip', 'wb') as zip_file:
-        zip_file.write(code_data['Location'].read())
+    func_details = lambda_client.get_function(FunctionName=function_name)
+    zip_file='./' + function_name + '.zip'
+    code_data = func_details['Code']['Location']
+
+    r = requests.get(code_data)
+    with open(zip_file, 'wb') as code:
+        code.write(r.content)
 
 
 if __name__ == "__main__":
     get_function_from_source()
-
